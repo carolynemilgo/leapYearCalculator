b495bb0878c95f3854f760592312aa0056522e31
carolynemilgo <carolynemilgo@gmail.com>
2017-06-07 23:18:18 +0300

    A program to calculate leap years

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("form#leapYear").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapyear(year);
+    //  $("#result").text(result);
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+    $("#result").show();
+  });
+});
+
+var leapyear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
